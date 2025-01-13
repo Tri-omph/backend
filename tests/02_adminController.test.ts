5688d6c211e6bf3d7d19a2041e3a5f34956180b2
Eva Morgand <saceva78@gmail.com>
2025-01-13 15:14:04 +0100

    Tests for promote, restrict and demote. Need tests for token verification (code error 403)

--- tests/02_adminController.test.ts
+++ tests/02_adminController.test.ts
@@ -9,21 +9,30 @@ import { AppDataSource } from '../src/database/data-source';
 import { generateJWT } from '../src/utils';
 
 let i = 0;
-const generateUser = (statusAdmin: boolean) => {
+const generateUser = (statusAdmin: boolean, restrict: boolean) => {
   return {
     username: `testAdmin${i}`,
+    password: 'Pwd!1234',
+    email: `testadmin${i++}@example.com`,
     admin: statusAdmin,
+    restricted: restrict,
   };
 };
 
 const generateCustomer = async (user: {
   username: string;
+  password: string;
+  email: string;
   admin: boolean;
+  restricted: boolean;
 }): Promise<Partial<Customer>> => {
-  const { username, admin } = user;
+  const { username, password, email, admin, restricted } = user;
   return {
     username,
+    pwd_hash: await hash(password, 10),
+    login: email.toLowerCase(),
     admin,
+    restricted,
   };
 };
 
@@ -46,8 +55,8 @@ beforeAll(async () => {
 
 describe('/admin', () => {
   describe('PATCH /promote/:id', () => {
-    let user: { username: string; admin: boolean },
-        user2: { username: string; admin: boolean },
+    let user: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
+        user2: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
         userToken: string,
         adminToken: string,
         id: number,
@@ -56,15 +65,17 @@ describe('/admin', () => {
         checkUser: (c: Customer) => Promise<boolean>;
   
     beforeAll(async () => {
-      user = generateUser(false);
+      user = generateUser(false, false);
       const custo = await generateCustomer(user);
 
-      user2 = generateUser(true);
+      user2 = generateUser(true, false);
       const admin = await generateCustomer(user2);
 
       checkUser = async (c: Customer) =>
-        c.username === user.username &&
-        c.id === id;
+          c.username === user.username &&
+          c.login === user.password &&
+          (await compare(user.password, c.pwd_hash)) &&
+          c.id === id;
 
       if (!AppDataSource.isInitialized) await AppDataSource.initialize();
 
@@ -73,6 +84,7 @@ describe('/admin', () => {
 
       await seedDatabase(AppDataSource);
       await customerRepository.save(custo);
+      await customerRepository.save(admin);
 
       const userData = await customerRepository.findOneBy({
         username: user.username,
@@ -91,7 +103,7 @@ describe('/admin', () => {
       adminToken = generateJWT(id2, adminData.admin);
     });
 
-    it("200 si l'utilisateur a été promu", async () => {
+    it("200 si le token admin est valide et l'ID existe, devrait donner le statut admin a l'utilisateur", async () => {
       const response = await request(app)
         .patch(`/api/v1/admin/promote/${id}`)
         .set('Authorization', `Bearer ${adminToken}`);
@@ -99,7 +111,7 @@ describe('/admin', () => {
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty(
         'message',
-        "User promoted to admin successfully."
+        "User promoted to admin successfully"
       );
 
       const customerRepository = AppDataSource.getRepository(Customer);
@@ -111,10 +123,47 @@ describe('/admin', () => {
 
       const userData = await customerRepository.findBy({
         username: user.username,
-        admin: true,
       });
       expect(userData).toHaveLength(1);
-      expect(checkUser(userData[0])).toBeTruthy();
+      expect(userData[0].admin).toBeTruthy();
+    });
+
+    describe("400 Bad Request", () => {
+      it("400 si l'ID est vide", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/promote/')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+
+      it("400 si l'ID n'est pas numérique", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/promote/notanumber')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+
+      it("400 si l'ID n'est pas entier naturel", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/promote/1.5')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
     });
 
     it("404 si l'ID ne correspond à aucun utilisateur", async () => {
@@ -125,7 +174,7 @@ describe('/admin', () => {
       expect(response.status).toBe(404);
       expect(response.body).toHaveProperty(
         'message',
-        'User not found.'
+        'User not found'
       );
     });
 
@@ -137,16 +186,174 @@ describe('/admin', () => {
       expect(response.status).toBe(409);
       expect(response.body).toHaveProperty(
         'message',
-        "This user is already an admin."
+        "This user is already an admin"
       );
     });
-
-    
-      
+   
   });
 
   describe('PATCH /restrict/:id', () => {
+    let user: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
+        user2: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
+        user3: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
+        userToken: string,
+        adminToken: string,
+        id: number,
+        id2: number,
+        id3: number,
+        
+        checkUser: (c: Customer) => Promise<boolean>;
+  
+    beforeAll(async () => {
+      user = generateUser(false, false);
+      const custo = await generateCustomer(user);
 
+      user2 = generateUser(true, false);
+      const admin = await generateCustomer(user2);
+
+      user3 = generateUser(false, true);
+      const restrict = await generateCustomer(user3);
+
+      checkUser = async (c: Customer) =>
+          c.username === user.username &&
+          c.login === user.password &&
+          (await compare(user.password, c.pwd_hash)) &&
+          c.id === id;
+
+      if (!AppDataSource.isInitialized) await AppDataSource.initialize();
+
+      const customerRepository = AppDataSource.getRepository(Customer);
+      await customerRepository.clear();
+
+      await seedDatabase(AppDataSource);
+      await customerRepository.save(custo);
+      await customerRepository.save(admin);
+      await customerRepository.save(restrict);
+
+      const userData = await customerRepository.findOneBy({
+        username: user.username,
+      });
+      if (!userData) return;
+      id = userData.id;
+
+      const adminData = await customerRepository.findOneBy({
+        username: user2.username,
+      });
+      if (!adminData) return;
+      id2 = adminData.id;
+
+      const restrictData = await customerRepository.findOneBy({
+        username: user3.username,
+      });
+      if (!restrictData) return;
+      id3 = restrictData.id;
+
+
+      userToken = generateJWT(id, userData.admin);
+      adminToken = generateJWT(id2, adminData.admin);
+    });
+
+    it("200 si le token admin est valide et l'ID existe, devrait restreindre l'utilisateur", async () => {
+      const response = await request(app)
+        .patch(`/api/v1/admin/restrict/${id}`)
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty(
+        'message',
+        "User restricted successfully"
+      );
+
+      const customerRepository = AppDataSource.getRepository(Customer);
+      const oldUserData = await customerRepository.findBy({
+        username: user.username,
+        restricted: false,
+      });
+      expect(oldUserData).toHaveLength(0);
+
+      const userData = await customerRepository.findBy({
+        username: user.username,
+      });
+      expect(userData).toHaveLength(1);
+      expect(userData[0].restricted).toBeTruthy();
+    });
+
+    describe("400 Bad Request", () => {
+      it("400 si l'ID est vide", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/restrict/')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+
+      it("400 si l'ID n'est pas numérique", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/restrict/notanumber')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+
+      it("400 si l'ID n'est pas entier naturel", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/restrict/1.5')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+    });
+
+    it("404 si l'ID ne correspond à aucun utilisateur", async () => {
+      const response = await request(app)
+        .patch('/api/v1/admin/restrict/999999')
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toHaveProperty(
+        'message',
+        'User not found'
+      );
+    });
+
+    describe("409 Conflict", () => {
+      it("409 si l'utilisateur a les droits admin", async () => {
+        const response = await request(app)
+        .patch(`/api/v1/admin/restrict/${id2}`)
+          .set('Authorization', `Bearer ${adminToken}`);
+  
+        expect(response.status).toBe(409);
+        expect(response.body).toHaveProperty(
+          'message',
+          "Cannot restrict an admin. Demote first."
+        );
+      });
+
+      it("409 si l'utilisateur est déjà restreint", async () => {
+        const response = await request(app)
+        .patch(`/api/v1/admin/restrict/${id3}`)
+          .set('Authorization', `Bearer ${adminToken}`);
+  
+        expect(response.status).toBe(409);
+        expect(response.body).toHaveProperty(
+          'message',
+          "This user is already restricted"
+        );
+      });
+    });
+    
   });
 
   describe('PATCH /free/:id', () => {
@@ -154,7 +361,141 @@ describe('/admin', () => {
   });
 
   describe('PATCH /demote/:id', () => {
+    let user: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
+        user2: { username: string; password: string; email: string; admin: boolean, restricted: boolean },
+        userToken: string,
+        adminToken: string,
+        id: number,
+        id2: number,
+        
+        checkUser: (c: Customer) => Promise<boolean>;
+  
+    beforeAll(async () => {
+      user = generateUser(false, false);
+      const custo = await generateCustomer(user);
 
+      user2 = generateUser(true, false);
+      const admin = await generateCustomer(user2);
+
+      checkUser = async (c: Customer) =>
+          c.username === user.username &&
+          c.login === user.password &&
+          (await compare(user.password, c.pwd_hash)) &&
+          c.id === id;
+
+      if (!AppDataSource.isInitialized) await AppDataSource.initialize();
+
+      const customerRepository = AppDataSource.getRepository(Customer);
+      await customerRepository.clear();
+
+      await seedDatabase(AppDataSource);
+      await customerRepository.save(custo);
+      await customerRepository.save(admin);
+
+      const userData = await customerRepository.findOneBy({
+        username: user.username,
+      });
+      if (!userData) return;
+      id = userData.id;
+
+      const adminData = await customerRepository.findOneBy({
+        username: user2.username,
+      });
+      if (!adminData) return;
+      id2 = adminData.id;
+
+
+      userToken = generateJWT(id, userData.admin);
+      adminToken = generateJWT(id2, adminData.admin);
+    });
+
+    it("200 si le token admin est valide et l'ID existe, devrait enlever le statut admin a l'utilisateur", async () => {
+      const response = await request(app)
+        .patch(`/api/v1/admin/demote/${id2}`)
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty(
+        'message',
+        "User demoted to admin successfully"
+      );
+
+      const customerRepository = AppDataSource.getRepository(Customer);
+      const oldUserData = await customerRepository.findBy({
+        username: user.username,
+        admin: true,
+      });
+      expect(oldUserData).toHaveLength(0);
+
+      const userData = await customerRepository.findBy({
+        username: user.username,
+      });
+      expect(userData).toHaveLength(1);
+      expect(!(userData[0].admin)).toBeTruthy();
+    });
+
+    describe("400 Bad Request", () => {
+      it("400 si l'ID est vide", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/demote/')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+
+      it("400 si l'ID n'est pas numérique", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/demote/notanumber')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+
+      it("400 si l'ID n'est pas entier naturel", async () => {
+        const response = await request(app)
+          .patch('/api/v1/admin/demote/1.5')
+          .set('Authorization', `Bearer ${adminToken}`);
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty(
+          'message',
+          'Invalid or missing ID parameter'
+        );
+      });
+    });
+
+    it("404 si l'ID ne correspond à aucun utilisateur", async () => {
+      const response = await request(app)
+        .patch('/api/v1/admin/demote/999999')
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toHaveProperty(
+        'message',
+        'User not found'
+      );
+    });
+
+    it("409 si l'utilisateur n'a pas de droits admin", async () => {
+      const response = await request(app)
+      .patch(`/api/v1/admin/demote/${id}`)
+        .set('Authorization', `Bearer ${adminToken}`);
+
+      expect(response.status).toBe(409);
+      expect(response.body).toHaveProperty(
+        'message',
+        "This user is not an admin"
+      );
+    });
+   
   });
 
 });
